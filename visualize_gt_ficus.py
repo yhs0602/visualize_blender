d6ede2e36e0ca7b614181149dc90f7c3b8fa3082
yhs0602 <jourhyang123@gmail.com>
2024-03-06 00:57:07 +0900

    :sparkles: Gt ficus

---
+++ visualize_gt_ficus.py
@@ -0,0 +1,28 @@
+import os
+
+from generalized_hotdog import render_mesh
+
+blender_nums = {
+    "hotdog": 40,
+    "ship": 144,
+    "mic": 144,
+    "chair": 64,
+    "materials": 152,  # 19
+    "ficus": 136,  # 17
+}
+
+if __name__ == "__main__":
+    dir_name = "gt"
+    for file in os.listdir(dir_name):
+        print(file)
+        if not file.endswith(".ply"):
+            continue  # .DS_Store
+        # algorithm, obj, cleaned = file.split(".")[0].split("_")
+        # obj = file.split(".")[0]
+        # print(f"{algorithm=}{obj=}")
+        if file.endswith(".ply"):
+            render_mesh(
+                filename=f"{dir_name}/{file}",
+                blender_num=blender_nums["ficus"],
+                # camera_angle_x=camera_angle_x
+            )
